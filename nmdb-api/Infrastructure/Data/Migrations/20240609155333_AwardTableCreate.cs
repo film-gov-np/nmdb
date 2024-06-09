0ba1bde1d5cb7a0539c1ed0321f10b990103d61f
sudhirsth <sudhirsth03@gmail.com>
2024-06-09 21:42:25 +0545

    Award talbe created.

---
+++ nmdb-api/Infrastructure/Data/Migrations/20240609155333_AwardTableCreate.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AwardTableCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Crews",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Awards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AwardTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AwardedIn = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AwardStatus = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AwardedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Remarks = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CrewID = table.Column<int>(type: "int", nullable: true),
+                    MovieID = table.Column<int>(type: "int", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Awards", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Awards_Crews_CrewID",
+                        column: x => x.CrewID,
+                        principalTable: "Crews",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Awards_Movies_MovieID",
+                        column: x => x.MovieID,
+                        principalTable: "Movies",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Crews_Email",
+                table: "Crews",
+                column: "Email",
+                unique: true,
+                filter: "[Email] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Awards_CrewID",
+                table: "Awards",
+                column: "CrewID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Awards_MovieID",
+                table: "Awards",
+                column: "MovieID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Awards");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Crews_Email",
+                table: "Crews");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Crews",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+        }
+    }
+}
