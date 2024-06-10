afe747ee78076efb513dcf33f35b41e7b9060dcc
niraz-st <niraz.st@gmail.com>
2024-06-10 23:37:23 +0545

    front celebrities pages

--- nmdb-web-vite/src/constants/authConstant.js
+++ nmdb-web-vite/src/constants/authConstant.js
@@ -1,4 +1,4 @@
 export const BaseAPIUrl = "https://api.nepalidev.com.np/api/";
 // export const BaseAPIUrl = "https://localhost:7037/api/";
-export const ServerPath = "https://localhost:7037/";
+export const ServerPath = "https://api.nepalidev.com.np/";
 
