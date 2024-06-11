56f10a1ac26c77e0a517ea993a23bf008aa8d1c2
niraz-st <niraz.st@gmail.com>
2024-06-11 21:58:31 +0545

    test

--- nmdb-web-vite/src/constants/authConstant.js
+++ nmdb-web-vite/src/constants/authConstant.js
@@ -1,5 +1,5 @@
-export const BaseAPIUrl = "https://api.nepalidev.com.np/api/";
-export const ServerPath = "https://api.nepalidev.com.np/";
+export const BaseAPIUrl = "https://nmdb.azurewebsites.net/api/";
+export const ServerPath = "https://nmdb.azurewebsites.net/";
 
 // export const BaseAPIUrl = "https://localhost:7037/api/";
 // export const ServerPath = "https://localhost:7037/";
