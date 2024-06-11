f1e4039d7ef2802410a04ffbdccf2be08812e8ee
niraz-st <niraz.st@gmail.com>
2024-06-11 21:49:03 +0545

    test

--- nmdb-web-vite/src/constants/authConstant.js
+++ nmdb-web-vite/src/constants/authConstant.js
@@ -1,4 +1,6 @@
 export const BaseAPIUrl = "https://api.nepalidev.com.np/api/";
-// export const BaseAPIUrl = "https://localhost:7037/api/";
 export const ServerPath = "https://api.nepalidev.com.np/";
+
+// export const BaseAPIUrl = "https://localhost:7037/api/";
+// export const ServerPath = "https://localhost:7037/";
 
