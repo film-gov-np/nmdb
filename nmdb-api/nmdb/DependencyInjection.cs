f70aefbb78f4f40cbcd62dea3abe9fa6bbb16d44
Sushil Shrestha <sushil@sageflick.com>
2024-06-12 14:10:14 +0545

    fix(minor): change for test only

--- nmdb-api/nmdb/DependencyInjection.cs
+++ nmdb-api/nmdb/DependencyInjection.cs
@@ -19,7 +19,6 @@ public static class DependencyInjection
             options.AddPolicy("AllowSpecificOrigin",
                 builder =>
                 {
-                    // Cors
                     builder.WithOrigins("http://localhost:5173", "https://nmdb-phi.vercel.app", "https://nmdb.azurewebsites.net")
                            .AllowAnyHeader()
                            .AllowAnyMethod()
