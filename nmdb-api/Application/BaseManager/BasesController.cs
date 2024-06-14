0484434f24b25f791e2210c73ff0b77fea65f359
Sushil Shrestha <shoesheill@gmail.com>
2024-06-14 09:14:10 +0545

    chore: update template dependecies

--- nmdb-api/Application/BaseManager/BasesController.cs
+++ nmdb-api/Application/BaseManager/BasesController.cs
@@ -1,39 +1,40 @@
 ﻿using Application.Response;
+using Core;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System.Web;
 
-namespace Application.BaseManager
+namespace Application
 {
     public class BaseAPIController : ControllerBase
     {
-        private readonly APIResponse aPIResponse;
+        private readonly ApiResponse aPIResponse;
         protected readonly ILogger<BaseAPIController> _logger;
         private readonly IHttpContextAccessor _httpContextAccessor;
-        public BaseAPIController(APIResponse aPIResponse, ILogger<BaseAPIController> _logger, IHttpContextAccessor httpContextAccessor)
+        public BaseAPIController(ApiResponse aPIResponse, ILogger<BaseAPIController> _logger, IHttpContextAccessor httpContextAccessor)
         {
             this.aPIResponse = aPIResponse;
             this._logger = _logger;
             this._httpContextAccessor = httpContextAccessor;
         }
 
-        [NonAction]
-        public OkObjectResult OnSuccess(object data, int status_code = 200)
-        {
-            return Ok(this.aPIResponse.APISuccessResponse(data, status_code));
-        }
-        [NonAction]
-        public BadRequestObjectResult OnBadRequest(string message, string error_type, int status_code = 400)
-        {
-            return BadRequest(this.aPIResponse.ApiErrorResponse(message, error_type, status_code));
-        }
-        [NonAction]
-        public NotFoundObjectResult OnNotFound(string message, string error_type, int status_code = 404)
-        {
-            return NotFound(this.aPIResponse.ApiErrorResponse(message, error_type, status_code));
-        }
+        // [NonAction]
+        // public OkObjectResult OnSuccess(object data, int status_code = 200)
+        // {
+        //     return Ok(this.aPIResponse.APISuccessResponse(data, status_code));
+        // }
+        // [NonAction]
+        // public BadRequestObjectResult OnBadRequest(string message, string error_type, int status_code = 400)
+        // {
+        //     return BadRequest(this.aPIResponse.ApiErrorResponse(message, error_type, status_code));
+        // }
+        // [NonAction]
+        // public NotFoundObjectResult OnNotFound(string message, string error_type, int status_code = 404)
+        // {
+        //     return NotFound(this.aPIResponse.ApiErrorResponse(message, error_type, status_code));
+        // }
 
         public string CurrentUserID
         {
@@ -54,6 +55,13 @@ namespace Application.BaseManager
                 Core.Data user = (Core.Data?)_httpContextAccessor.HttpContext.Items["user"];
                 userID = user?.Email;
                 return userID;
+            }
+        }
+        public string GetHostUrl
+        {
+            get
+            {
+                return $"{this.Request.Scheme}://{this.Request.Host}{this.Request.PathBase}";
             }
         }
 
--- nmdb-api/Core/Constants/EmailTemplate.cs
+++ nmdb-api/Core/Constants/EmailTemplate.cs
@@ -116,6 +116,30 @@ public static class EmailTemplate
             </html>";
         return emailTemplate;
     }
+    public static string PasswordResetEmail(string username, string resetToken)
+    {
+        var emailTemplate = $@"
+        <!DOCTYPE html>
+            <html>
+            <head>
+              {getEmailStyle()}
+            </head>
+                <body>
+                   <div class='email-container'>
+                       <h2>Password Reset Request</h2>
+                                <p>Dear ${username},</p>
+                                <p>You have requested to reset your password. Please click the button below to set a new password:</p>
+                                    <a href='{resetToken}' class='button'>Reset Password</a>
+                                <p>If the button above does not work, please copy and paste the following URL into your browser:</p>
+                                <p>{resetToken}</p>
+                                <p>If you did not request a password reset, please ignore this email or contact support.</p>
+                        </div>
+                    <div>${getCompanySignature()}</div>
+                </body>
+            </html>";
+        return emailTemplate;
+
+    }
     private static string getCompanySignature()
     {
         return "";
