b3a783eceb88deb8be8d6b8ea09a42db08eaa28d
sudhirsth <sudhirsth03@gmail.com>
2024-06-14 15:41:24 +0545

    Users api profile photo update.

--- nmdb-api/Infrastructure/Identity/Services/IUserService.cs
+++ nmdb-api/Infrastructure/Identity/Services/IUserService.cs
@@ -1,4 +1,6 @@
-﻿using Application.Dtos.User;
+﻿using Application.Dtos.FilterParameters;
+using Application.Dtos.User;
+using Application.Helpers.Response;
 using Core;
 
 namespace Infrastructure.Identity.Services;
@@ -6,7 +8,9 @@ namespace Infrastructure.Identity.Services;
 public interface IUserService
 {
     Task<ApiResponse<string>> CreateUserAsync(UserRequestDto userRequest);
-    Task<ApiResponse<string>> UpdateUserAsync(UserRequestDto userRequest);
+    Task<ApiResponse<string>> UpdateUserAsync(UserUpdateRequestDto userRequest);
     Task<ApiResponse<string>> DeleteUserAsync(string userId);
+    Task<ApiResponse<UserResponseDto>> GetUserById(string userId);
+    Task<ApiResponse<PaginationResponse<UserResponseDto>>> GetUsers(BaseFilterParameters filterParameters);
     Task<ApiResponse<string>> UpdateUserRoleAsync(string userId, string roleId);
 }
--- nmdb-api/Infrastructure/Identity/Services/UserService.cs
+++ nmdb-api/Infrastructure/Identity/Services/UserService.cs
@@ -1,14 +1,23 @@
-﻿using Application.Dtos.User;
+﻿using Application.Dtos.FilterParameters;
+using Application.Dtos.Media;
+using Application.Dtos.Movie;
+using Application.Dtos.User;
+using Application.Helpers;
+using Application.Helpers.Response;
+using Application.Interfaces.Services;
 using Application.Models;
 using AutoMapper;
 using Core;
 using Core.Constants;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace Infrastructure.Identity.Services;
 
@@ -16,15 +25,22 @@ public class UserService : IUserService
 {
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly RoleManager<ApplicationRole> _roleManager;
+    private readonly IFileService _fileService;
+    private readonly IHttpContextAccessor _httpContextAccessor;
     private readonly IMapper _mapper;
+    private const string userSubDirectory = "/upload/img/users/";
 
     public UserService(UserManager<ApplicationUser> userManager,
         RoleManager<ApplicationRole> roleManager,
+        IHttpContextAccessor httpContextAccessor,
+        IFileService fileService,
         IMapper mapper)
     {
         _userManager = userManager;
         _roleManager = roleManager;
         _mapper = mapper;
+        _fileService=fileService;
+        _httpContextAccessor = httpContextAccessor;
     }
 
     public async Task<ApiResponse<string>> CreateUserAsync(UserRequestDto userRequest)
@@ -35,6 +51,23 @@ public class UserService : IUserService
             user.CreatedBy = userRequest.Authorship;
             user.UserName = userRequest.Email;
 
+
+            if (userRequest.ProfilePhotoFile != null)
+            {
+                FileDTO fileDto = new FileDTO
+                {
+                    Files = userRequest.ProfilePhotoFile,
+                    Thumbnail = false,
+                    ReadableName = false,
+                    SubFolder = userSubDirectory
+                };
+                var uploadResult = await _fileService.UploadFile(fileDto);
+                if (uploadResult.IsSuccess && uploadResult.Data != null)
+                {
+                    user.ProfilePhoto = uploadResult.Data.FileName;
+                }
+            }
+
             if (string.IsNullOrEmpty(userRequest.Role))
                 userRequest.Role = AuthorizationConstants.UserRole;
 
@@ -44,7 +77,9 @@ public class UserService : IUserService
             {
                 return ApiResponse<string>.ErrorResponse($"Role '{roleToBeAssigned?.Name}'  does not exist in the database.");
             }
+
             var userCreateResult = await _userManager.CreateAsync(user, userRequest.Password);
+
             var errorMessage = "";
             if (userCreateResult.Succeeded)
             {
@@ -66,7 +101,7 @@ public class UserService : IUserService
         }
     }
 
-    public async Task<ApiResponse<string>> UpdateUserAsync(UserRequestDto userRequest)
+    public async Task<ApiResponse<string>> UpdateUserAsync(UserUpdateRequestDto userRequest)
     {
         try
         {
@@ -79,6 +114,24 @@ public class UserService : IUserService
 
             _mapper.Map(userRequest, existingUser);
 
+            if (userRequest.ProfilePhotoFile != null)
+            {
+                FileDTO fileDto = new FileDTO
+                {
+                    Files = userRequest.ProfilePhotoFile,
+                    Thumbnail = false,
+                    ReadableName = true,
+                    SubFolder = userSubDirectory
+                };
+                var uploadResult = await _fileService.UploadFile(fileDto);
+                if (uploadResult.IsSuccess && uploadResult.Data != null)
+                {
+                    if (!string.IsNullOrEmpty(userRequest.ProfilePhoto))
+                        _fileService.RemoveFile(userRequest.ProfilePhoto, fileDto.SubFolder);
+
+                    existingUser.ProfilePhoto = uploadResult.Data.FileName;
+                }
+            }
             if (!string.IsNullOrEmpty(userRequest.Role))
             {
                 var roleToBeAssigned = await _roleManager.FindByNameAsync(userRequest.Role);
@@ -175,6 +228,76 @@ public class UserService : IUserService
         catch (Exception ex)
         {
             return ApiResponse<string>.ErrorResponse(ex.Message);
+        }
+    }
+
+    public async Task<ApiResponse<UserResponseDto>> GetUserById(string userId)
+    {
+        try
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+                return ApiResponse<UserResponseDto>.ErrorResponse($"User with id '{userId}' not found.");
+            var userResponse = _mapper.Map<UserResponseDto>(user);
+            if (!string.IsNullOrEmpty(user.ProfilePhoto))
+            {
+                var hostUrl = ImageUrlHelper.GetHostUrl(_httpContextAccessor);
+                userResponse.ProfilePhotoUrl = ImageUrlHelper.GetFullImageUrl(hostUrl, userSubDirectory, user.ProfilePhoto);
+            }
+            return ApiResponse<UserResponseDto>.SuccessResponse(userResponse);
+        }
+        catch (Exception ex)
+        {
+            return ApiResponse<UserResponseDto>.ErrorResponse(ex.Message);
+        }
+    }
+
+    public async Task<ApiResponse<PaginationResponse<UserResponseDto>>> GetUsers(BaseFilterParameters filterParameters)
+    {
+        try
+        {
+            var query = _userManager.Users.AsQueryable();
+
+            if (!string.IsNullOrEmpty(filterParameters.SearchKeyword))
+            {
+                query = query.Where(u => u.Email.Contains(filterParameters.SearchKeyword) ||
+                u.FirstName.Contains(filterParameters.SearchKeyword) ||
+                u.LastName.Contains(filterParameters.SearchKeyword));
+            }
+
+            var totalCount = await query.CountAsync();
+            var hostUrl = ImageUrlHelper.GetHostUrl(_httpContextAccessor);
+
+            // fetch only certain columns with pagination
+            var users = await query
+                .Select(u => new UserResponseDto
+                {
+                    Id = u.Id,
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                    Email = u.Email,
+                    PhoneNumber = u.PhoneNumber,
+                    ProfilePhotoUrl = ImageUrlHelper.GetFullImageUrl(hostUrl, userSubDirectory, u.ProfilePhoto)
+                })
+                .Skip((filterParameters.PageNumber - 1) * filterParameters.PageSize)
+                .Take(filterParameters.PageSize)
+                .ToListAsync();
+                        
+            var userResponseDtos = users.Select(user => _mapper.Map<UserResponseDto>(user)).ToList();
+
+            var pagedResult = new PaginationResponse<UserResponseDto>
+            {
+                Items = userResponseDtos,
+                TotalItems = totalCount,
+                PageNumber = filterParameters.PageNumber,
+                PageSize = filterParameters.PageSize
+            };
+
+            return ApiResponse<PaginationResponse<UserResponseDto>>.SuccessResponse(pagedResult);
+        }
+        catch (Exception ex)
+        {
+            return ApiResponse<PaginationResponse<UserResponseDto>>.ErrorResponse(ex.Message);
         }
     }
 }
