3625f277c2b7faf4a8e7f937a1cd20bc04f6275e
sudhirsth <sudhirsth03@gmail.com>
2024-06-20 00:12:41 +0545

    Remove faceted filter from the users table.

--- nmdb-web-vite/src/components/admin/pages/user/User.jsx
+++ nmdb-web-vite/src/components/admin/pages/user/User.jsx
@@ -3,30 +3,13 @@ import { Paths } from "@/constants/routePaths";
 import { ApiPaths } from "@/constants/apiPaths";
 import { DataTableAdvancedServerControlled } from "@/components/ui/custom/data-table/data-table-advanced-server-controlled";
 
-export const facetedFilters = [{
-  name: "emailConfirmed",
-  title: "Email Confirmed",
-  accessorKey:"EmailConfirmed",
-  filters: [
-    {
-      value: true,
-      label: "Yes",
-    },
-    {
-      value: false,
-      label: "No",
-    },
-  ],
-}];
-
 const User = () => {
 
   return (
     <main className="flex flex-1 flex-col gap-2 overflow-auto p-4 lg:gap-4 lg:p-6">
       <DataTableAdvancedServerControlled
         apiPath={ApiPaths.Path_Users}
-        columns={columns}
-        facetedFilters={facetedFilters}
+        columns={columns}        
         queryKey="datatable-user"
         nameLabel="user"
         addNewPath={Paths.Route_Admin_User_Add}
