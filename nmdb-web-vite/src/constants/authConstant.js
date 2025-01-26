a7bbc73f70a799a8a6e92d80f9f928c48dc234cf
Sushil Shrestha <shoesheill@gmail.com>
2025-01-26 22:47:12 +0545

    chore(api-url):change api url to local url

--- nmdb-web-vite/src/constants/authConstant.js
+++ nmdb-web-vite/src/constants/authConstant.js
@@ -1,5 +1,5 @@
-export const BaseAPIUrl = "https://nmdb-api.nepalidev.com.np/api/";
-export const ServerPath = "https://nmdb-api.nepalidev.com.np/";
+export const BaseAPIUrl = "http://10.5.4.49:5001/api/";
+export const ServerPath = "http://10.5.4.49:5001/";
 
 //export const BaseAPIUrl = "https://localhost:7037/api/";
 //export const ServerPath = "https://localhost:7037/";
