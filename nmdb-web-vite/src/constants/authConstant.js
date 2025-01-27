149521cf609a2b7031ec85f647ad80f85ff8fe9e
Sushil Shrestha <shoesheill@gmail.com>
2025-01-27 19:53:21 +0545

    chore(api-url): update api url route

--- nmdb-web-vite/src/constants/authConstant.js
+++ nmdb-web-vite/src/constants/authConstant.js
@@ -1,5 +1,5 @@
-export const BaseAPIUrl = "http://10.5.4.49:5001/api/";
-export const ServerPath = "http://10.5.4.49:5001/";
+export const BaseAPIUrl = "https://nmdb-api.film.gov.np/api/";
+export const ServerPath = "https://nmdb-api.film.gov.np/";
 
 //export const BaseAPIUrl = "https://localhost:7037/api/";
 //export const ServerPath = "https://localhost:7037/";
